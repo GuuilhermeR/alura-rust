ea0780b0c6b2d2fac0063e16379b68bc598fd5ff
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-05-31 18:19:52 -0300

    04. Detalhes do Rust

--- Principal/main.rs
+++ Principal/main.rs
@@ -2,8 +2,7 @@ const PI:f32 = 3.14; //const precisa sempre informar o valor.
 //static VARIAVEL_GLOBAL:u8 = 1; -- variável global usar o static (static = const, ambas precisam ter valores definidos), como mutáveis é inseguro.
 static mut VARIAVEL_GLOBAL:u8 = 1;// variável global usar o static (static = const, ambas precisam ter valores definidos), como mutáveis é inseguro.
 
-fn soma(a:i32, b:i32) -> i32 // Parâmetros precisa informar o tipo. O retorno é "mostrado" com "->" e o tipo do retorno.
-{
+fn soma(a:i32, b:i32) -> i32{// Parâmetros precisa informar o tipo. O retorno é "mostrado" com "->" e o tipo do retorno.
     println!("{} + {} = {}", a, b, a + b);
     a + b // para retornar não utiliza o ;. Quando omito o ; ele me dá o retorno, quando utilizo eu sinalizo que é o fim da expressão e ignoro. Pode utilizar return também.
 }
@@ -57,6 +56,11 @@ fn main(){
 
     condicionais();
     repeticoes();
+
+    ownership();
+
+    pattern_matching();
+    erros();
 }
 
 fn condicionais() {
@@ -128,4 +132,52 @@ fn repeticoes(){
 
         println!("{} x {} = {}", multiplicador, i, multiplicador * i);
     }
+}
+
+fn ownership(){ //somente uma variável pode ser dona do que temos na heap, só que uma_string pode "emprestar" (empresta pra ele e dps devolve)
+    let mut uma_string = String::from("Guilherme"); // ele pega o conjunto de caracteres e converte para um objeto. Eu sei que é texto estático mas transforma como se fosse dinamico
+    rouba(&mut uma_string); //deixo de receber string e recebo a referência da string
+    
+    println!("{}", uma_string); //Valor na heap é um só, o que é copiado de um pro outro é o ponteiro.
+}
+
+fn rouba(string: &mut String) {
+    string.push_str(" Rudiger");
+    println!("{}", string);
+}
+
+fn pattern_matching(){
+    for x in 1..=20{
+        println!("{}: {}", x, match x {
+            1 => "Pouco",
+            2 | 3 => "Um pouquinho",
+            4..=10 => "Um bocado",
+            _ if x % 2 == 0 => "Uma boa quantidade", //valores pares
+            _ => "Muito"
+        });
+    }
+
+    // match point{
+    //     (0,0) => "origem",
+    //     (0,_) => "eixo x",
+    //     (_,0) => "eixo x"
+    // }
+}
+
+fn erros(){
+    match resultado() {
+        Ok(s) => println!("String de sucesso é essa = {}", s),
+        Err(numero) => println!("Código de erro = {}", numero) //pode usar também o panic!("Código de erro = {}", numero)
+    };
+
+    //panic!("Erro proposital!"); //Para quando ocorrer o erro usar essa função.
+
+    // let v = vec![1,2,3]; //vetor
+    // v[4]; // tentar pegar um vetor com índice 4.
+}
+
+fn resultado() -> Result<String, u8>
+{
+    // Ok(String::from("Tudo deu certo"))
+    Err(42)
 }
